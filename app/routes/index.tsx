13016c6dc68d0287d51116ea0b7c8309f5a426bd
Mikéias Oliveira <mykeias2_bk@hotmail.com>
2022-02-23 18:54:21 -0300

    ihaha

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -22,7 +22,7 @@ export default function Index() {
             href="https://remix.run/tutorials/blog"
             rel="noreferrer"
           >
-            15m Quickstart Blog Tutorial
+            15m Quickstart Blog Tutorialss
           </a>
         </li>
         <li>
